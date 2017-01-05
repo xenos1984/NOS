cbb6d8662fec5fe61d9b2fad9c94103a32448a68
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-01-05 10:13:02 +0200

    :warning: Make sure toolchain is always linked to executable path, also when used from cache.

--- ci/toolchain.sh
+++ ci/toolchain.sh
@@ -8,64 +8,62 @@ newlib_version="2.4.0"
 target=$1
 prefix=~/cross/$target
 
-if [ -d $prefix ]
+if [ ! -d $prefix ]
 then
-	exit 0
+	mkdir -p /tmp/toolchain
+	cd /tmp/toolchain
+
+	if [ ! -f gcc-$gcc_version.tar.bz2 ]
+	then
+		wget -c -O gcc-$gcc_version.tar.bz2 ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version/gcc-$gcc_version.tar.bz2
+		tar -xf gcc-$gcc_version.tar.bz2
+	fi
+
+	if [ ! -f binutils-$binutils_version.tar.bz2 ]
+	then
+		wget -c -O binutils-$binutils_version.tar.bz2 ftp://ftp.gnu.org/gnu/binutils/binutils-$binutils_version.tar.bz2
+		tar -xf binutils-$binutils_version.tar.bz2
+	fi
+
+	if [ ! -f newlib-$newlib_version.tar.gz ]
+	then
+		wget -c -O newlib-$newlib_version.tar.gz ftp://sources.redhat.com/pub/newlib/newlib-$newlib_version.tar.gz
+		tar -xf newlib-$newlib_version.tar.gz
+	fi
+
+	mkdir -p /tmp/toolchain/build-binutils
+	mkdir -p /tmp/toolchain/build-gcc
+	mkdir -p /tmp/toolchain/build-newlib
+
+	cd /tmp/toolchain/build-binutils
+	sudo rm -rf *
+	/tmp/toolchain/binutils-$binutils_version/configure --target=$target --prefix=$prefix --disable-nls 2>&1
+	make all 2>&1
+	make install 2>&1
+	sudo rm -rf *
+
+	cd /tmp/toolchain/build-gcc
+	/tmp/toolchain/gcc-$gcc_version/configure --target=$target --prefix=$prefix --disable-nls --enable-languages=c,c++ --enable-libstdcxx --without-headers 2>&1
+	make all-gcc 2>&1
+	make install-gcc 2>&1
+	make all-target-libgcc 2>&1
+	make install-target-libgcc 2>&1
+
+	sudo ln -s -f $prefix/bin/* /usr/local/bin/
+
+	cd /tmp/toolchain/build-newlib
+	sudo rm -rf *
+	/tmp/toolchain/newlib-$newlib_version/configure --target=$target --prefix=$prefix 2>&1
+	make all 2>&1
+	make install 2>&1
+	sudo rm -rf *
+
+	cd /tmp/toolchain/build-gcc
+	/tmp/toolchain/gcc-$gcc_version/configure --target=$target --prefix=$prefix --disable-nls --enable-languages=c,c++ --enable-libstdcxx --without-headers --with-newlib 2>&1
+	make all-target-libstdc++-v3 2>&1
+	make install-target-libstdc++-v3 2>&1
+	sudo rm -rf *
 fi
-
-mkdir -p /tmp/toolchain
-cd /tmp/toolchain
-
-if [ ! -f gcc-$gcc_version.tar.bz2 ]
-then
-	wget -c -O gcc-$gcc_version.tar.bz2 ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version/gcc-$gcc_version.tar.bz2
-	tar -xf gcc-$gcc_version.tar.bz2
-fi
-
-if [ ! -f binutils-$binutils_version.tar.bz2 ]
-then
-	wget -c -O binutils-$binutils_version.tar.bz2 ftp://ftp.gnu.org/gnu/binutils/binutils-$binutils_version.tar.bz2
-	tar -xf binutils-$binutils_version.tar.bz2
-fi
-
-if [ ! -f newlib-$newlib_version.tar.gz ]
-then
-	wget -c -O newlib-$newlib_version.tar.gz ftp://sources.redhat.com/pub/newlib/newlib-$newlib_version.tar.gz
-	tar -xf newlib-$newlib_version.tar.gz
-fi
-
-mkdir -p /tmp/toolchain/build-binutils
-mkdir -p /tmp/toolchain/build-gcc
-mkdir -p /tmp/toolchain/build-newlib
-
-cd /tmp/toolchain/build-binutils
-sudo rm -rf *
-/tmp/toolchain/binutils-$binutils_version/configure --target=$target --prefix=$prefix --disable-nls 2>&1
-make all 2>&1
-make install 2>&1
-sudo rm -rf *
-
-cd /tmp/toolchain/build-gcc
-/tmp/toolchain/gcc-$gcc_version/configure --target=$target --prefix=$prefix --disable-nls --enable-languages=c,c++ --enable-libstdcxx --without-headers 2>&1
-make all-gcc 2>&1
-make install-gcc 2>&1
-make all-target-libgcc 2>&1
-make install-target-libgcc 2>&1
-
-sudo ln -s -f $prefix/bin/* /usr/local/bin/
-
-cd /tmp/toolchain/build-newlib
-sudo rm -rf *
-/tmp/toolchain/newlib-$newlib_version/configure --target=$target --prefix=$prefix 2>&1
-make all 2>&1
-make install 2>&1
-sudo rm -rf *
-
-cd /tmp/toolchain/build-gcc
-/tmp/toolchain/gcc-$gcc_version/configure --target=$target --prefix=$prefix --disable-nls --enable-languages=c,c++ --enable-libstdcxx --without-headers --with-newlib 2>&1
-make all-target-libstdc++-v3 2>&1
-make install-target-libstdc++-v3 2>&1
-sudo rm -rf *
 
 sudo ln -s -f $prefix/bin/* /usr/local/bin/
 
