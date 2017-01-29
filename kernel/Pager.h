e4758f41f4f0cd1f1c5c79fe3fd005a46260c52f
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-01-29 20:39:45 +0200

    :heavy_plus_sign: Documented the tasks done by some pager functions.

--- kernel/Pager.h
+++ kernel/Pager.h
@@ -10,12 +10,28 @@ namespace Kernel
 {
 	namespace Pager
 	{
+		/** Map a single physical page to a virtual address and assign page table flags suitable for a given memory type. */
 		template<Memory::PageBits bits> void MapPage(Memory::PhysAddr phys, uintptr_t virt, Memory::MemType type);
+
+		/** Unmap a page from a virtual address. */
 		template<Memory::PageBits bits> void UnmapPage(uintptr_t virt);
+
+		/** Check the size of a page mapped to a given address. */
 		Memory::PageBits MappedSize(uintptr_t virt);
+
+		/** Check whether a particular physical memory range is completely mapped. */
 		bool IsMapped(uintptr_t virt, size_t length);
+
+		/** Check whether a particular physical memory range is completely unmapped. */
 		bool IsUnmapped(uintptr_t virt, size_t length);
+
+		/** Map a range of physical to virtual memory and and assign page table flags suitable for a given memory type. */
 		bool Map(Memory::PhysAddr phys, uintptr_t virt, size_t length, Memory::MemType type);
+
+		/** Unmap a range of virtual memory. */
+		bool Unmap(uintptr_t virt, size_t length);
+
+		/** Get physical address to which a virtual address is mapped. */
 		Memory::PhysAddr VirtToPhys(uintptr_t virt);
 		Memory::PhysAddr VirtToPhys(void* virt);
 	}
