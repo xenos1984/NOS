c4324cfc58f63d90e7577ab83c7cbd1c091307b4
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-03-05 10:28:47 +0200

    :warning: Fix some compiler warnings for operator delete.

--- kernel/heap/Heap.cpp
+++ kernel/heap/Heap.cpp
@@ -1,6 +1,7 @@
 // Heap.cpp - Kernel heap management
 
 #include <Heap.h>
+#include <cstddef>
 #include <new>
 
 void* operator new(size_t count)
@@ -13,22 +14,22 @@ void* operator new[](size_t count)
 	return Kernel::Heap::Alloc(count);
 }
 
-void operator delete(void* pointer)
+void operator delete(void* pointer) noexcept
 {
 	Kernel::Heap::Free(pointer);
 }
 
-void operator delete[](void* pointer)
+void operator delete[](void* pointer) noexcept
 {
 	Kernel::Heap::Free(pointer);
 }
 
-void operator delete(void* pointer, size_t size)
+void operator delete(void* pointer, size_t size __attribute__((unused)))
 {
 	Kernel::Heap::Free(pointer);
 }
 
-void operator delete[](void* pointer, size_t size)
+void operator delete[](void* pointer, size_t size __attribute__((unused)))
 {
 	Kernel::Heap::Free(pointer);
 }
