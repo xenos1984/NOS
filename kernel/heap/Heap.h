d9a26891dcfe374ea031a2c81c7e862875669f1b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-03-05 10:47:24 +0200

    :heavy_plus_sign: Helper functions to allocate and map / unmap and free pages in the heap.

--- kernel/heap/Heap.h
+++ kernel/heap/Heap.h
@@ -4,6 +4,8 @@
 #define __HEAP_HEAP_H__
 
 #include <Heap.h>
+#include <Chunker.h>
+#include <Pager.h>
 #include <cstdint>
 
 namespace Kernel
@@ -23,6 +25,37 @@ namespace Kernel
 		inline uintptr_t RoundDiff(uintptr_t addr, uintptr_t align = sizeof(long))
 		{
 			return align - 1 - ((addr - 1) & (align - 1));
+		}
+
+		void AllocBlock(uintptr_t addr)
+		{
+			Memory::PhysAddr phys = Chunker::Alloc(static_cast<Memory::Zone>(0));
+			Pager::MapPage<Memory::MinPageBits>(phys, addr, Memory::MemType::KERNEL_RW);
+		}
+
+		void FreeBlock(uintptr_t addr)
+		{
+			Memory::PhysAddr phys = Pager::VirtToPhys(addr);
+			Pager::UnmapPage<Memory::MinPageBits>(addr);
+			Chunker::Free(phys);
+		}
+
+		void AllocBlocks(uintptr_t addr, unsigned int count)
+		{
+			for(unsigned int i = 0; i < count; i++)
+			{
+				AllocBlock(addr);
+				addr += (1 << Memory::MinPageBits);
+			}
+		}
+
+		void FreeBlocks(uintptr_t addr, unsigned int count)
+		{
+			for(unsigned int i = 0; i < count; i++)
+			{
+				FreeBlock(addr);
+				addr += (1 << Memory::MinPageBits);
+			}
 		}
 	}
 }
