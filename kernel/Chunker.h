68111fa14f26342802ffd1f0b04a37cfa14824b3
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-04-30 01:15:37 +0300

    :heavy_plus_sign: Add brief description to chunker functions.

--- kernel/Chunker.h
+++ kernel/Chunker.h
@@ -9,13 +9,21 @@ namespace Kernel
 {
 	namespace Chunker
 	{
+		/** Initialize the chunker with the first area of available memory. */
 		void Init(Memory::PhysAddr start, Memory::PhysAddr length, Memory::Zone zone);
+		/** Add another region of available memory to the chunker. */
 		void AddRegion(Memory::PhysAddr start, Memory::PhysAddr length, Memory::Zone zone);
 
+		/** Find a free chunk of a given size and allocate it. */
 		template<Memory::PageBits bits> Memory::PhysAddr Alloc(Memory::Zone zone = static_cast<Memory::Zone>(0));
-		bool Free(Memory::PhysAddr addr);
+		/** Mark a chunk of memory as free. */
+		template<Memory::PageBits bits> bool Free(Memory::PhysAddr addr);
+		/** Mark a chunk of memory as reserved. */
+		template<Memory::PageBits bits> bool Reserve(Memory::PhysAddr addr);
+
+		/** Mark a range of memory as free. */
 		bool Free(Memory::PhysAddr first, Memory::PhysAddr last);
-		bool Reserve(Memory::PhysAddr addr);
+		/** Mark a range of memory as reserved. */
 		bool Reserve(Memory::PhysAddr first, Memory::PhysAddr last);
 	}
 }
