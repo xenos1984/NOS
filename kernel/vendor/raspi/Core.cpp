9ea12e6e2913ad7940da903bb3da2e02c3bee0b6
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-09-24 21:06:05 +0300

    :heavy_plus_sign: Add simple data abort stub for testing purposes.

--- kernel/vendor/raspi/Core.cpp
+++ kernel/vendor/raspi/Core.cpp
@@ -20,5 +20,7 @@ extern "C" void SECTION(".init.text") KernelEntry(uint32_t r0, uint32_t r1, uint
 	Console::WriteFormat("SCTLR = 0x%8x\n", Coprocessor::SCTLR::Read());
 	Console::WriteFormat("MIDR = 0x%8x\n", Coprocessor::MIDR::Read());
 	Console::WriteFormat("VBAR = 0x%8x\n", Coprocessor::VBAR::Read());
+
+	*((volatile int*)0x12345678) = 0x87654321;
 }
 
