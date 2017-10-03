c26b156ff53e1926f5e80f1bd16e341a5d7719bd
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-04 00:03:08 +0300

    :warning: Put QEMU log files in correct artifact folder.

--- .circleci/qemu_raspi.sh
+++ .circleci/qemu_raspi.sh
@@ -4,5 +4,5 @@ dir=$1
 
 shift 1
 
-sh -c 'sleep 30; echo "quit"' | qemu-system-arm -m 256 -M raspi2 -cpu cortex-a7 -display none -monitor stdio -serial file:$dir/qemu.txt -kernel $dir/Kernel.elf -d cpu_reset -D qemu.log $@
+sh -c 'sleep 30; echo "quit"' | qemu-system-arm -m 256 -M raspi2 -cpu cortex-a7 -display none -monitor stdio -serial file:$dir/qemu.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu.log $@
 
