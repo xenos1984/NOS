c112802484ba038328ca5126925a9e2a55bf051c
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-10 01:23:24 +0300

    :warning: Workaround - install netcat.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+apt-get install netcat
+
 printf "NICK :nos-circleci\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :#nos\r\nPRIVMSG #nos :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 irc.freenode.net 6667
