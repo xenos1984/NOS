a00b6cefb0b58712f7e407d61874c7232577d10e
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-10 23:33:40 +0300

    :warning: Use netcat as a temporary IRC workaround.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# printf "NICK :nos-circleci\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :#nos\r\nPRIVMSG #nos :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 irc.freenode.net 6667
-
 server=irc.freenode.net
 port=6667
 channel="#nos"
+
+printf "NICK :nos-circleci\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :$channel\r\nPRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 $server $port
+
+exit 0
 
 ii -s $server -p $port -i /root -n nos-circleci -f "NOS CircleCI bot" &
 
