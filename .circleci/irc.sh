9429f397442e07bcb2bf8a34bf86a01591592793
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-10 22:41:39 +0300

    :warning: Use docker image with ii for IRC notification.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-apt-get install netcat
+# printf "NICK :nos-circleci\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :#nos\r\nPRIVMSG #nos :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 irc.freenode.net 6667
 
-printf "NICK :nos-circleci\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :#nos\r\nPRIVMSG #nos :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 irc.freenode.net 6667
+server=irc.freenode.net
+port=6667
+channel="#nos"
+
+ii -s $server -p $port -i /tmp -n nos-circleci -f "NOS CircleCI bot" &
+
+sleep 30
+
+echo "/j $channel" > /tmp/$server/in
+echo "NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results." > "/tmp/$server/$channel/in"
+echo "/quit" > /tmp/$server/in
+
