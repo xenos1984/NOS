dfb69b066bdbb00307164abbe10c95387aceea43
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-11 23:50:13 +0300

    :warning: Try SASL authentication to IRC via netcat.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -5,14 +5,25 @@ port=6667
 channel="#nos"
 prefix=/root
 nick=nos-circleci
+base64=`printf "$nick\0$nick\0$IRCPASS" | base64`
+
+(
+	printf "CAP REQ :sasl\r\n"
+	printf "AUTHENTICATE PLAIN\r\n"
+	printf "AUTHENTICATE $base64\r\n"
+	printf "CAP END\r\n"
+	printf "NICK :nos-circleci\r\n"
+	printf "USER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\n"
+	printf "JOIN :$channel\r\n"
+	printf "PRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\n"
+	printf "QUIT :Bye!\r\n"
+) | nc -q 60 $server $port
+
+exit 0
 
 echo "NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results." > /root/irc.txt
 
 sh -c 'sleep 30; cat /root/irc.txt' | irc -d -c "$channel" $nick $server:$port:$IRCPASS
-
-#printf "NICK :nos-circleci\r\nPASS $IRCPASS\r\nUSER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\nJOIN :$channel\r\nPRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\nQUIT :Bye!\r\n" | nc -q 60 $server $port
-
-exit 0
 
 ii -s $server -p $port -i $prefix -n nos-circleci -f "NOS CircleCI bot" -k IRCPASS &
 
