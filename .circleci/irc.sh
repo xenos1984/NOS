df75a72cbae92117c26e6a9b495ad2c47a2dabc9
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-10-12 20:24:13 +0300

    :heavy_plus_sign: Show job in IRC notification.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -15,7 +15,7 @@ base64=`printf "$nick\0$nick\0$IRCPASS" | base64`
 	printf "NICK :nos-circleci\r\n"
 	printf "USER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\n"
 	printf "JOIN :$channel\r\n"
-	printf "PRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM finished - see $CIRCLE_BUILD_URL for results.\r\n"
+	printf "PRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM - $CIRCLE_JOB finished - see $CIRCLE_BUILD_URL for results.\r\n"
 	printf "QUIT :Bye!\r\n"
 ) | nc -q 60 $server $port
 
