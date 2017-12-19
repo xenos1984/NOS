f77c497b9e5530742d6989ad4f726cfcacc716cc
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2017-12-20 00:01:27 +0200

    :warning: Corrected location of build files.

--- .circleci/pc_multi_iso.sh
+++ .circleci/pc_multi_iso.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+$folder=~/nos/$1
+
 grub-mkimage -o eltorito.img -O i386-pc-eltorito biosdisk iso9660 multiboot configfile
 
-genisoimage -R -c boot/boot.cat -b boot/grub/eltorito.img -no-emul-boot -boot-load-size 4 -boot-info-table -o $1/NOS.iso -graft-points \
+genisoimage -R -c boot/boot.cat -b boot/grub/eltorito.img -no-emul-boot -boot-load-size 4 -boot-info-table -o $folder/NOS.iso -graft-points \
 	boot/grub/eltorito.img=eltorito.img \
 	boot/grub/grub.cfg=.circleci/grub.cfg \
-	nos/mp-acpi/Kernel.elf=$1/mp-acpi/Kernel.elf \
-	nos/mp-noacpi/Kernel.elf=$1/mp-noacpi/Kernel.elf \
-	nos/sp-acpi/Kernel.elf=$1/sp-acpi/Kernel.elf \
-	nos/sp-noacpi/Kernel.elf=$1/sp-noacpi/Kernel.elf
+	nos/mp-acpi/Kernel.elf=$folder/mp-acpi/Kernel.elf \
+	nos/mp-noacpi/Kernel.elf=$folder/mp-noacpi/Kernel.elf \
+	nos/sp-acpi/Kernel.elf=$folder/sp-acpi/Kernel.elf \
+	nos/sp-noacpi/Kernel.elf=$folder/sp-noacpi/Kernel.elf
 
