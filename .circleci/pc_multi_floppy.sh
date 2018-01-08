ba575cc312e9396436b0c899f7c49da33673c78c
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-01-08 23:14:03 +0200

    :heavy_plus_sign: Automatically create bootable floppy image (first attempt).

---
+++ .circleci/pc_multi_floppy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+folder=~/nos/$1
+loop=`losetup -f`
+image=$folder/NOS.img
+mnt=/mnt/floppy
+
+dd if=/dev/zero of=$image bs=512 count=2880
+losetup $loop $image
+mke2fs $loop
+mkdir -p $mnt
+mount $loop $mnt
+mkdir -p $mnt/boot $mnt/nos
+grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="biosdev multiboot configfile ext2" --force --allow-floppy $loop
+cp .circleci/grub.cfg $mnt/boot/grub/
+for conf in mp-acpi mp-noacpi sp-acpi sp-noacpi
+do
+	mkdir -p $mnt/nos/$conf
+	cp $folder/$conf/Kernel.elf $mnt/nos/$conf/
+done
+umount $loop
+sync
+losetup -d $loop
+bzip2 $image
+
