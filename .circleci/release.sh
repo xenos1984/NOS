55c21a9700a8caacffc26c60f6b0d7d63b34810c
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-01-09 19:43:35 +0200

    :heavy_plus_sign: Automatically create bootable floppy image (second attempt - try machine executor).

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -15,11 +15,11 @@ done
 curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686.iso&label=NOS%20bootable%20ISO%20%2Ei686%2F"
 
 # i686 floppy image
-#curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686.img.bz2&label=NOS%20bootable%20floppy%20%2Ei686%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686.img.bz2&label=NOS%20bootable%20floppy%20%2Ei686%2F"
 
 # x86_64 CD image
 curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64.iso&label=NOS%20bootable%20ISO%20%2Ex86_64%2F"
 
 # x86_64 floppy image
-#curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64.img.bz2&label=NOS%20bootable%20floppy%20%2Ex86_64%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64.img.bz2&label=NOS%20bootable%20floppy%20%2Ex86_64%2F"
 
