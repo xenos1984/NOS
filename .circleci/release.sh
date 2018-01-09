96e98870665a59b1f0d5caeeb6af21d312f9b61d
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-01-10 00:00:08 +0200

    :warning: Change . and / to () in asset labels.

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -12,20 +12,20 @@ done
 # Upload freshly built release assets.
 
 # i686 CD image
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686.iso&label=NOS%20bootable%20ISO%20%2Ei686%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686.iso&label=NOS%20bootable%20ISO%20%28i686%29"
 
 # i686 floppy image (FAT12)
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS-fat12.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686-fat12.img.bz2&label=NOS%20bootable%20floppy%20%2Ei686-FAT12%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS-fat12.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686-fat12.img.bz2&label=NOS%20bootable%20floppy%20%28i686-FAT12%29"
 
 # i686 floppy image (EXT2)
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS-ext2.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686-ext2.img.bz2&label=NOS%20bootable%20floppy%20%2Ei686-EXT2%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @i686-pc-elf/NOS-ext2.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-i686-ext2.img.bz2&label=NOS%20bootable%20floppy%20%28i686-EXT2%29"
 
 # x86_64 CD image
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64.iso&label=NOS%20bootable%20ISO%20%2Ex86_64%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS.iso "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64.iso&label=NOS%20bootable%20ISO%20%28x86_64%29"
 
 # x86_64 floppy image (FAT12)
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS-fat12.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64-fat12.img.bz2&label=NOS%20bootable%20floppy%20%2Ex86_64-FAT12%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS-fat12.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64-fat12.img.bz2&label=NOS%20bootable%20floppy%20%28x86_64-FAT12%29"
 
 # x86_64 floppy image (EXT2)
-curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS-ext2.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64-ext2.img.bz2&label=NOS%20bootable%20floppy%20%2Ex86_64-EXT2%2F"
+curl -X POST -u xenos1984:$GHTOKEN -H 'Content-Type: application/octet-stream' --data-binary @x86_64-pc-elf/NOS-ext2.img.bz2 "https://uploads.github.com/repos/xenos1984/NOS/releases/9063364/assets?name=nos-x86_64-ext2.img.bz2&label=NOS%20bootable%20floppy%20%28x86_64-EXT2%29"
 
