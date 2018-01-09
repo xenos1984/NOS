6a2929c69dd79fb46a7269dca1f50be7d611c510
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-01-09 20:59:49 +0200

    :warning: Automatically create bootable floppy image (eighth attempt - try FAT12 instead of EXT2).

--- .circleci/pc_multi_floppy.sh
+++ .circleci/pc_multi_floppy.sh
@@ -7,11 +7,11 @@ mnt=/mnt/floppy
 
 dd if=/dev/zero of=$image bs=512 count=2880
 losetup $loop $image
-mke2fs $loop
+mkdosfs -F 12 $loop
 mkdir -p $mnt
 mount $loop $mnt
 mkdir -p $mnt/boot $mnt/nos
-grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile ext2" --fonts=ascii --force --allow-floppy $loop
+grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile fat" --fonts=ascii --force --allow-floppy $loop
 cp ~/nos/source/.circleci/grub.cfg $mnt/boot/grub/
 for conf in mp-acpi mp-noacpi sp-acpi sp-noacpi
 do
