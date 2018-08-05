0fca8543c662d8804a3c03d0b81b058543dbb4bc
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-08-06 00:23:15 +0300

    :heavy_plus_sign: Automatically build aarch64-raspi3-elf kernel and test on QEMU.

--- .circleci/qemu_raspi.sh
+++ .circleci/qemu_raspi.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-dir=$1
+arch=$1
+mach=$2
+cpu=$3
+dir=$4
 
-shift 1
+shift 4
 
-sh -c 'sleep 10; echo "quit"' | qemu-system-arm -m 256 -M raspi2 -cpu cortex-a7 -display none -monitor stdio -serial file:$dir/qemu.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu.log $@
+sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m 256 -M $mach -cpu $cpu -display none -monitor stdio -serial file:$dir/qemu-$mach.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu-$mach.log $@
 
