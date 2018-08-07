eecedd4c91c6d7109a759970d65a5ddab2d21a4b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2018-08-08 00:37:12 +0300

    :heavy_plus_sign: Flags for page table entries on VMSAv8-64.

---
+++ kernel/arch/arm/v8/PageTableEntry.h
@@ -0,0 +1,45 @@
+// PageTableEntry.h - Single entry in a page table.
+
+#ifndef __ARCH_ARM_V8_PAGETABLEENTRY_H__
+#define __ARCH_ARM_V8_PAGETABLEENTRY_H__
+
+#include <type_traits>
+#include <Memory.h>
+#include <Pager.h>
+
+namespace Kernel
+{
+	namespace Pager
+	{
+		class PageTableEntry
+		{
+		private:
+			uint64_t raw = 0;
+
+			enum Flags : uint64_t
+			{
+				PAGE_INVALID    = 0x0000000000000000,
+				PAGE_BLOCK      = 0x0000000000000001,
+				PAGE_TABLE      = 0x0000000000000003,
+				PAGE_PAGE       = 0x0000000000000003,
+				PAGE_ATTR_MASK  = 0x000000000000001c,
+				PAGE_NSECURE    = 0x0000000000000020,
+				PAGE_KERNEL     = 0x0000000000000000,
+				PAGE_USER       = 0x0000000000000040,
+				PAGE_RW         = 0x0000000000000000,
+				PAGE_RO         = 0x0000000000000080,
+				PAGE_SH_NONE    = 0x0000000000000000,
+				PAGE_SH_OUTER   = 0x0000000000000200,
+				PAGE_SH_INNER   = 0x0000000000000300,
+				PAGE_ACCESSED   = 0x0000000000000400,
+				PAGE_NGLOBAL    = 0x0000000000000800,
+				PAGE_DIRTY      = 0x0008000000000000,
+				PAGE_CONTIGUOUS = 0x0010000000000000,
+				PAGE_K_NOEXEC   = 0x0020000000000000,
+				PAGE_U_NOEXEC   = 0x0040000000000000
+			};
+		};
+	}
+}
+
+#endif
