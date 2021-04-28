145e0ce287bfa3142c0ec07e879964b477f0a3d2
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2021-04-29 00:06:33 +0300

    :warning: Change user to execute image creation.

--- .circleci/pc_multi_floppy.sh
+++ .circleci/pc_multi_floppy.sh
@@ -12,43 +12,43 @@ mnt=/mnt/floppy
 #bunzip2 $image.bz2
 
 dd if=/dev/zero of=$image bs=512 count=2880
-losetup $loop $image
+sudo losetup $loop $image
 
 case $fs in
 	fat12)
-		mkdosfs -F 12 $loop
+		sudo mkdosfs -F 12 $loop
 		;;
 	ext2)
-		mke2fs $loop
+		sudo mke2fs $loop
 		;;
 	*)
 		exit 1
 		;;
 esac
 
-mkdir -p $mnt
-mount $loop $mnt
-mkdir -p $mnt/nos $mnt/boot
+sudo mkdir -p $mnt
+sudo mount $loop $mnt
+sudo mkdir -p $mnt/nos $mnt/boot
 
 case $fs in
 	fat12)
-		grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile fat" --fonts=ascii --force --allow-floppy $loop
+		sudo grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile fat" --fonts=ascii --force --allow-floppy $loop
 		;;
 	ext2)
-		grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile ext2" --fonts=ascii --force --allow-floppy $loop
+		sudo grub-install --target=i386-pc --boot-directory=$mnt/boot --install-modules="multiboot configfile ext2" --fonts=ascii --force --allow-floppy $loop
 		;;
 	*)
 		exit 1
 		;;
 esac
 
-cp ~/nos/source/.circleci/grub.cfg $mnt/boot/grub/
+sudo cp ~/nos/source/.circleci/grub.cfg $mnt/boot/grub/
 for conf in mp-acpi mp-noacpi sp-acpi sp-noacpi
 do
-	mkdir -p $mnt/nos/$conf
-	cp $folder/$conf/Kernel.elf $mnt/nos/$conf/
+	sudo mkdir -p $mnt/nos/$conf
+	sudo cp $folder/$conf/Kernel.elf $mnt/nos/$conf/
 done
-umount $loop
-sync
-losetup -d $loop
+sudo umount $loop
+sudo sync
+sudo losetup -d $loop
 bzip2 $image
