a46500536d90e10fb8d360d6346452e89278fbb9
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2021-06-15 15:27:41 +0300

    :arrows_clockwise: Changed automatic IRC notification to libera.chat.

--- .circleci/irc.sh
+++ .circleci/irc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server=irc.freenode.net
+server=irc.libera.chat
 port=6667
 channel="#nos"
 prefix=/root
@@ -8,14 +8,19 @@ nick=nos-circleci
 base64=`printf "$nick\0$nick\0$IRCPASS" | base64`
 
 (
+	sleep 10
 	printf "CAP REQ :sasl\r\n"
 	printf "AUTHENTICATE PLAIN\r\n"
 	printf "AUTHENTICATE $base64\r\n"
 	printf "CAP END\r\n"
+	sleep 5
 	printf "NICK :nos-circleci\r\n"
 	printf "USER nos-circleci nos-circleci nos-circleci :NOS CircleCI bot\r\n"
+	sleep 5
 	printf "JOIN :$channel\r\n"
+	sleep 5
 	printf "PRIVMSG $channel :NOS build #$CIRCLE_BUILD_NUM - $CIRCLE_JOB finished - see $CIRCLE_BUILD_URL for results.\r\n"
+	sleep 5
 	printf "QUIT :Bye!\r\n"
-) | nc -q 60 $server $port
+) | nc -q 15 $server $port
 
