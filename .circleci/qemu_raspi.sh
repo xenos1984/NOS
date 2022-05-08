8f77278d0d21b30b74884d8a53389f4b8268f767
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2022-05-08 09:15:03 +0300

    :warning: Fixed CI Qemu run script.

--- .circleci/qemu_raspi.sh
+++ .circleci/qemu_raspi.sh
@@ -6,7 +6,6 @@ cpu=$3
 ram=$4
 dir=$5
 
-shift 4
+shift 5
 
 sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m $ram -M $mach -cpu $cpu -display none -monitor stdio -serial file:$dir/qemu-$mach.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu-$mach.log $@
-
