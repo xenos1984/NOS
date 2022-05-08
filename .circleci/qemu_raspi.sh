283576ef90d265667456c0cc6c52910e0901aaad
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2022-05-08 09:10:31 +0300

    :warning: Fixed Qemu RPi RAM size.

--- .circleci/qemu_raspi.sh
+++ .circleci/qemu_raspi.sh
@@ -3,9 +3,10 @@
 arch=$1
 mach=$2
 cpu=$3
-dir=$4
+ram=$4
+dir=$5
 
 shift 4
 
-sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m 1024 -M $mach -cpu $cpu -display none -monitor stdio -serial file:$dir/qemu-$mach.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu-$mach.log $@
+sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m $ram -M $mach -cpu $cpu -display none -monitor stdio -serial file:$dir/qemu-$mach.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu-$mach.log $@
 
