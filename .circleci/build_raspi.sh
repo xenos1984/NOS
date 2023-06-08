8dad9e64678f0a10ddecf2325acfee77ba8c071b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-09 00:28:19 +0300

    :warning: Correct tool prefix in CI script.

--- .circleci/build_raspi.sh
+++ .circleci/build_raspi.sh
@@ -10,11 +10,11 @@ make 2>&1 | tee build.log
 ansifilter -H -d "Build log $target" -i build.log -o build.html
 sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" build.log
 
-$prefix=`grep '^ac_tool_prefix' config.log | sed -e "s/^.*'\(.*\)-'$/\1/"`
+prefix=`grep '^ac_tool_prefix' config.log | sed -e "s/^.*'\(.*\)-'$/\1/"`
 mkdir -p $folder
 cp config.log $folder/
 cp build.log $folder/
-cp build.log.html $folder/
+cp build.html $folder/
 cp kernel/vendor/raspi/Kernel.elf $folder/
 cp kernel/vendor/raspi/Kernel.map $folder/
 #cp images/raspi/NOS.img $folder/
