68f93530518005006dad0a95c3ae54b99ce8a161
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-08 19:49:33 +0300

    :heavy_plus_sign: Show build log and kernel dump as colored HTML.

--- .circleci/build_pc.sh
+++ .circleci/build_pc.sh
@@ -4,8 +4,11 @@ target=$1
 folder=~/nos/$target/$2
 shift 2
 
+export CFLAGS="$CFLAGS -fdiagnostics-color=always"
 ./configure --host=$target $@
 make 2>&1 | tee build.log
+ansifilter -H -d "Build log $target" -i build.log -o build.html
+sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" build.log
 
 mkdir -p $folder
 cp config.log $folder/
@@ -14,6 +17,6 @@ cp kernel/vendor/pc/Kernel.elf $folder/
 cp kernel/vendor/pc/Kernel.map $folder/
 #cp drivers/pc/floppy/Floppy.elf $folder/
 cp images/pc/NOS.iso $folder/
-$target-objdump -x -d -C -g -S $folder/Kernel.elf > $folder/Kernel.dump
+$target-objdump -x -s -d -C -g -S --visualize-jumps=extended-color --disassembler-color=extended $folder/Kernel.elf | ansifilter -H -d "Dump $target" > $folder/Kernel.dump.html
 
 make clean
--- .circleci/build_raspi.sh
+++ .circleci/build_raspi.sh
@@ -4,8 +4,11 @@ target=$1
 folder=~/nos/$target/$2
 shift 2
 
+export CFLAGS="$CFLAGS -fdiagnostics-color=always"
 ./configure --host=$target $@
 make 2>&1 | tee build.log
+ansifilter -H -d "Build log $target" -i build.log -o build.html
+sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" build.log
 
 mkdir -p $folder
 cp config.log $folder/
@@ -13,6 +16,6 @@ cp build.log $folder/
 cp kernel/vendor/raspi/Kernel.elf $folder/
 cp kernel/vendor/raspi/Kernel.map $folder/
 #cp images/raspi/NOS.img $folder/
-$target-objdump -x -d -C -g -S $folder/Kernel.elf > $folder/Kernel.dump
+$target-objdump -x -s -d -C -g -S --visualize-jumps=extended-color --disassembler-color=extended $folder/Kernel.elf | ansifilter -H -d "Dump $target" > $folder/Kernel.dump.html
 
 make clean
