728d30f7a1ef997b9ab89435cf7db81975a86b8b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-09 19:26:01 +0300

    :heavy_plus_sign: Generate HTML artifacts from console output on CI.

--- .circleci/qemu_raspi.sh
+++ .circleci/qemu_raspi.sh
@@ -9,3 +9,4 @@ dir=$5
 shift 5
 
 sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m $ram -M $mach -cpu $cpu -display none -monitor stdio -serial file:$dir/qemu-$mach.txt -kernel $dir/Kernel.elf -d cpu_reset -D $dir/qemu-$mach.log $@
+ansifilter -H --art-tundra -i $dir/qemu-$mach.txt -o $dir/qemu-$mach.html
