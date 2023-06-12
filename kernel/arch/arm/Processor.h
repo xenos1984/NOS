c98f6d2ba64da9b0fc47908646c5a29f95843455
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-12 21:13:53 +0300

    :warning: Added missing processor class on ARM.

---
+++ kernel/arch/arm/Processor.h
@@ -0,0 +1,16 @@
+// Processor.h - Processor management.
+
+#ifndef __ARCH_ARM_PROCESSOR_H__
+#define __ARCH_ARM_PROCESSOR_H__
+
+#include <cstdint>
+
+namespace Kernel
+{
+	struct ProcessorData
+	{
+		unsigned int id;
+	};
+}
+
+#endif
