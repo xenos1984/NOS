cac48ef358066026fbbb9ddddce96f14734aa921
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-12 23:31:50 +0300

    :arrows_clockwise: Explicitly choose multiple sockets for CI QEMU PC to populate MP tables.

--- .circleci/qemu_pc.sh
+++ .circleci/qemu_pc.sh
@@ -7,5 +7,5 @@ dir=$4
 
 shift 4
 
-sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m 256 -M $mach -smp $smp -display none -monitor stdio -debugcon file:$dir/qemu-$mach-$smp.txt -drive file=$dir/NOS.iso,media=cdrom,id=d -boot order=d -d cpu_reset -D $dir/qemu-$mach-$smp.log $@
+sh -c 'sleep 10; echo "quit"' | qemu-system-$arch -m 256 -M $mach -smp cpus=$smp,cores=1,threads=1,sockets=$smp -display none -monitor stdio -debugcon file:$dir/qemu-$mach-$smp.txt -drive file=$dir/NOS.iso,media=cdrom,id=d -boot order=d -d cpu_reset -D $dir/qemu-$mach-$smp.log $@
 ansifilter -H -d "Architecture: $arch Machine: $mach Subtype: $dir CPU cores: $smp" --art-tundra -i $dir/qemu-$mach-$smp.txt -o $dir/qemu-$mach-$smp.html
