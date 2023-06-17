ad885f142db893efae8d5cdadcdb846200e4c1f0
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-06-17 22:14:05 +0300

    :heavy_plus_sign: Keep flat binary Raspberry Pi kernels as CI artifacts.

--- .circleci/build_raspi.sh
+++ .circleci/build_raspi.sh
@@ -17,6 +17,7 @@ cp build.log $folder/
 cp build.html $folder/
 cp kernel/vendor/raspi/Kernel.elf $folder/
 cp kernel/vendor/raspi/Kernel.map $folder/
+cp kernel/vendor/raspi/kernel.img $folder/
 #cp images/raspi/NOS.img $folder/
 $prefix-objdump -x -s -d -C -g -S --visualize-jumps=extended-color --disassembler-color=extended $folder/Kernel.elf | ansifilter -H -d "Dump $target $@" > $folder/Kernel.dump.html
 
