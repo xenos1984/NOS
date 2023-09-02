4ae6f762d0aaae91095ac4cc9920540de989039b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-09-02 23:30:45 +0300

    :heavy_plus_sign: Add Bochs as additional CI test.

---
+++ .circleci/bochs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+here=$(dirname "$0")
+bochs=$1
+smp=$2
+dir=$3
+
+shift 4
+
+$bochs -f $here/bochsrc "ata0-master: type=cdrom, path=$dir/NOS.iso, status=inserted" "cpu: count=$smp" "log: $dir/$bochs-$smp.log" > $dir/$bochs-$smp.txt &
+pid=$!
+sleep 30
+kill $pid
+ansifilter -H -d "Emulator: $bochs Subtype: $dir CPU cores: $smp" --art-tundra -i $dir/$bochs-$smp.txt -o $dir/$bochs-$smp.html
