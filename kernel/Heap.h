d81693d79ce21447e22655f41a4b5c87d987bc5b
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-09-02 14:50:44 +0300

    :heavy_plus_sign: Add functions to separate memory allocation from initialization.

--- kernel/Heap.h
+++ kernel/Heap.h
@@ -16,6 +16,16 @@ namespace Kernel
 		void Free(void* addr, bool phys = true);
 
 		void ShowMap(void);
+
+		template<typename T> inline T* Alloc(size_t n, bool phys = true)
+		{
+			return static_cast<T*>(Alloc(n * sizeof(T), alignof(T), phys));
+		}
+
+		template<typename T> inline void Free(T* ptr, bool phys = true)
+		{
+			Free(ptr, phys);
+		}
 	}
 }
 
