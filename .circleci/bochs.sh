00ead29ae9718c6c091d5cde8a3e5862dd72c665
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-09-02 23:56:04 +0300

    :warning: Don't kill yourself.

--- .circleci/bochs.sh
+++ .circleci/bochs.sh
@@ -10,5 +10,5 @@ shift 4
 $bochs -q -f $here/bochsrc "ata0-master: type=cdrom, path=$dir/NOS.iso, status=inserted" "cpu: count=$smp" "log: $dir/$bochs-$smp.log" > $dir/$bochs-$smp.txt &
 pid=$!
 sleep 30
-pkill bochs
+pkill $bochs
 ansifilter -H -d "Emulator: $bochs Subtype: $dir CPU cores: $smp" --art-tundra -i $dir/$bochs-$smp.txt -o $dir/$bochs-$smp.html
